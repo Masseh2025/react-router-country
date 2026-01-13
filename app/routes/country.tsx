fa3588077aeb0046915a93628068facaf6352eac
Masseh Ziaie <mas154572@gmail.com>
2026-01-12 20:23:42 -0500

    made country page responsive

--- app/routes/country.tsx
+++ app/routes/country.tsx
@@ -10,49 +10,64 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
 }
 export default function Country({ loaderData }: Route.ComponentProps) {
   const lang = Object.keys(loaderData[0].name.nativeName)[0];
-  console.log(lang);
+  const currencies = Object.keys(loaderData[0].currencies);
+  const languages = Object.values(loaderData[0].languages);
   return (
-    <main className="w-full h-screen bg-gray-50">
-      <div className="w-full max-w-7xl mx-auto p-4 bg-grey-400">
-        <button className="p-2 rounded-md shadow-2xl bg-white">
-          <Link to="/countries">Back</Link>
-        </button>
-        <img
-          src={loaderData[0].flags.svg}
-          alt={loaderData[0].name.common}
-          className="w-full h-full object-cover max-w-md"
-        />
-        <div>
-          <div>
-            <h2 className="font-bold text-2xl">
-              {loaderData[0]?.name.common || "N/A"}
-            </h2>
-            <p>
-              <span className="font-bold">Native name: </span>
-              {loaderData[0]?.name.nativeName[lang].common || "N/A"}
-            </p>
-            <p>
-              <span className="font-bold">Population: </span>
-              {loaderData[0]?.population || "N/A"}
-            </p>
-            <p>
-              <span className="font-bold">Region: </span>
-              {loaderData[0]?.region || "N/A"}
-            </p>
-            <p>
-              <span className="font-bold">Subregion: </span>
-              {loaderData[0]?.subregion || "N/A"}
-            </p>
-            <p>
-              <span className="font-bold">Capital: </span>
-              {loaderData[0]?.capital || "N/A"}
-            </p>
-          </div>
-          <div>
-            <p>
-              <span className="font-bold">Top level domain: </span>
-              {loaderData[0]?.tld || "N/A"}
-            </p>
+    <main className="w-full h-full min-h-screen bg-gray-50 flex justify-center items-center">
+      <div className="w-full max-w-7xl p-4 bg-grey-400 flex flex-col justify-center items-center">
+        <div className="flex flex-col  justify-center items-center max-w-md w-full">
+          <button className="p-2 rounded-md shadow-2xl bg-white mb-8 self-start">
+            <Link to="/countries">Back</Link>
+          </button>
+          <img
+            src={loaderData[0].flags.svg}
+            alt={loaderData[0].name.common}
+            className="w-full h-full object-cover max-w-md mb-8"
+          />
+          <div className="w-full flex flex-col md:flex-row md:justify-center md:items-center">
+            <div className="mb-4 md:mb-0">
+              <h2 className="font-bold text-2xl mb-4">
+                {loaderData[0]?.name.common || "N/A"}
+              </h2>
+              <p>
+                <span className="font-bold">Native name: </span>
+                {loaderData[0]?.name.nativeName[lang].common || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Population: </span>
+                {loaderData[0]?.population || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Region: </span>
+                {loaderData[0]?.region || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Subregion: </span>
+                {loaderData[0]?.subregion || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Capital: </span>
+                {loaderData[0]?.capital || "N/A"}
+              </p>
+            </div>
+            <div className="md:ml-8 justify-center">
+              <p>
+                <span className="font-bold">Top level domain: </span>
+                {loaderData[0]?.tld || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Currencies: </span>
+                {currencies
+                  .map(
+                    (currency: any) => loaderData[0].currencies[currency].name
+                  )
+                  .join(", ") || "N/A"}
+              </p>
+              <p>
+                <span className="font-bold">Languages: </span>
+                {languages.map((language: any) => language).join(", ") || "N/A"}
+              </p>
+            </div>
           </div>
         </div>
       </div>
